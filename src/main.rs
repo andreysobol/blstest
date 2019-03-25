0ad1dbe5a5511b7e583c14f0828082daf78b3a61
Andrey Sobol <baskerville@andreysobol.com>
2019-03-26 00:08:34 +0200

    add agr

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,25 @@ fn main() {
     let message = "Looooooooooooooooooooooooong";
     let sig = keypair.sign(&message.as_bytes());
     assert_eq!(keypair.verify(&message.as_bytes(), &sig), true);
+
+    let keypair2 = Keypair::<Bls12>::generate(&mut rng);
+    let message2 = "loooooooooooooooooooong message";
+    let signature2 = keypair2.sign(&message2.as_bytes());
+
+    let mut inputs = Vec::with_capacity(2);
+    let mut signatures = Vec::with_capacity(2);
+
+    inputs.push((keypair.public, message));
+    inputs.push((keypair2.public, message2));
+    signatures.push(sig);
+    signatures.push(signature2);
+
+    let asig = AggregateSignature::from_signatures(&signatures);
+    assert_eq!(
+        asig.verify(&inputs
+            .iter()
+            .map(|&(ref pk, ref m)| (pk, m.as_bytes()))
+            .collect()),
+        true
+    );
 }
